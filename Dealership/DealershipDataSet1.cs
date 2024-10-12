50a8a208a7d9bfa0fa5b7044c8e4ab0d762ecf39
RedmiBook <RedmiBook@KROLIK>
2024-10-12 05:20:52 +0300

    Добавьте файлы проекта.

---
+++ Dealership/DealershipDataSet1.cs
@@ -0,0 +1,18 @@
+﻿namespace Dealership
+{
+
+
+    partial class DealershipDataSet1
+    {
+    }
+}
+
+namespace Dealership.DealershipDataSet1TableAdapters
+{
+    partial class ClientsTableAdapter
+    {
+    }
+
+    public partial class OrdersChecksTableAdapter {
+    }
+}
